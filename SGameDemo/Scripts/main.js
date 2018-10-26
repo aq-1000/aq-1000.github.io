16dadfe9e2a321f95cef275b72b416896481c899
PanYoucheng <aq_1000@sina.com>
2018-10-26 17:12:06 +0800

    更新

--- SGameDemo/Scripts/main.js
+++ SGameDemo/Scripts/main.js
@@ -1,4 +1,5 @@
 let windowWidth = window.innerWidth, windowHeight = window.innerHeight;
+let gScale = 1.0;
 
 // Create aPixi Application
 let app = new PIXI.Application({width: windowWidth, height: windowHeight});
@@ -21,37 +22,47 @@ PIXI.loader
     .on("progress", function(){
         // do something when loading
     })
-    .load(_OnAssetsLoaded);
+    .load(onAssetsLoaded);
 
 // 背景图加载完成
-function _OnAssetsLoaded()
+function onAssetsLoaded()
 {
     let backgroundSprite = new PIXI.Sprite(PIXI.loader.resources["Assets/Images/Background.png"].texture);
     app.stage.addChild(backgroundSprite);
-    backgroundSprite.height = (windowWidth / backgroundSprite.width) * backgroundSprite.height;
+
+    // 调整背景图，铺满全屏，图片居中
+    gScale = windowWidth / backgroundSprite.width;
     backgroundSprite.width = windowWidth;
+    backgroundSprite.height = backgroundSprite.height * gScale;
+    if (backgroundSprite.height > windowHeight) 
+    {
+        backgroundSprite.y -= (backgroundSprite.height - windowHeight) / 2;
+    }
 
-    let aladdinSheets = PIXI.loader.resources["Assets/Images/Aladdin.json"].spritesheet;
-    let aladdinIdle = new PIXI.extras.AnimatedSprite(aladdinSheets.animations["Aladdin_Idle"]);
-    aladdinIdle.x = app.screen.width / 2;
-    aladdinIdle.y = app.screen.height / 2;
-    aladdinIdle.anchor.set(0.5);
-    aladdinIdle.animationSpeed = 0.05;
-    aladdinIdle.loop = true;
-//    aladdinIdle.onComplete = function(){ aladdinIdle.visible = false; };
-//    aladdinIdle.play();
-    aladdinIdle.visible = false;
-    app.stage.addChild(aladdinIdle);
+    // let aladdinSheets = PIXI.loader.resources["Assets/Images/Aladdin.json"].spritesheet;
+    // let aladdinIdle = new PIXI.extras.AnimatedSprite(aladdinSheets.animations["Aladdin_Idle"]);
+    // aladdinIdle.x = app.screen.width / 2;
+    // aladdinIdle.y = app.screen.height / 2;
+    // //   aladdinIdle.anchor.set(0.5);
+    // aladdinIdle.animationSpeed = 0.05;
+    // aladdinIdle.loop = true;
+    // //    aladdinIdle.onComplete = function(){ aladdinIdle.visible = false; };
+    // //    aladdinIdle.play();
+    // aladdinIdle.visible = false;
+    // app.stage.addChild(aladdinIdle);
+    var sheet = PIXI.loader.resources["Assets/Images/Aladdin.json"].spritesheet;
+    var aladdin = new Role("Aladdin", sheet, 200, app.screen.height / 2, gScale, gScale);
+    aladdin.CreateAnimation("Idle", 0.05, true, true);
+    aladdin.CreateAnimation("Hit", 0.05, false, false);
 
-    let aladdinHit = new PIXI.extras.AnimatedSprite(aladdinSheets.animations["Aladdin_Hit"]);
-    aladdinHit.x = 150;
-    aladdinHit.y = app.screen.height / 2;
-    aladdinHit.anchor.set(0.5);
-    aladdinHit.animationSpeed = 0.05;
-    aladdinHit.loop = true;
-//    animHit.onComplete = function(){ animHit.visible = false; };
-    aladdinHit.play();
-    app.stage.addChild(aladdinHit);
+    sheet = PIXI.loader.resources["Assets/Images/Aladdin.json"].spritesheet;
+    var aladdin2 = new Role("Aladdin", sheet, app.screen.width - 200, app.screen.height / 2, -gScale, gScale);
+    aladdin2.CreateAnimation("Idle", 0.05, true, true);
+    aladdin2.CreateAnimation("Hit", 0.05, false, false);
+
+    // sheet = PIXI.loader.resources["Assets/Images/Aladdin.json"].spritesheet;
+    // var aladdin3 = new Role("Aladdin", sheet, app.screen.width - 150, app.screen.height / 2);
+    // aladdin3.CreateAnimation("Skill", 0.05, true, true);
 
     // let aladdinAttack = new PIXI.extras.AnimatedSprite(aladdinSheets.animations["Civ_SSR_Aladdin_00_Skill"]);
     // aladdinAttack.x = app.screen.width / 2;
--- SGameDemo/Scripts/role.js
+++ SGameDemo/Scripts/role.js
@@ -1,8 +1,39 @@
-(function(w)
+function Role(name, sheet, x, y, scaleX, scaleY)
 {
-    var Role = function(x, y)
+    this.name = name;
+    this.sheet = sheet;
+    this.x = x;
+    this.y = y;
+    this.scaleX = scaleX;
+    this.scaleY = scaleY;
+}
+
+Role.prototype = {
+    CreateAnimation:function(aniName, speed, loop, visible)
     {
-        this.x = x;
-        this.y = y;
+        var ani = new PIXI.extras.AnimatedSprite(this.sheet.animations[this.name + "_" + aniName]);
+        ani.animationSpeed = speed;
+        ani.loop = loop;
+    //    var.onComplete = function(){ aladdinIdle.visible = false; };
+        ani.x = this.x;
+        ani.y = this.y;
+        ani.scale.x = this.scaleX;
+        ani.scale.y = this.scaleY;
+        ani.anchor.set(0.5);
+        ani.visible = visible;
+        ani.play();
+        app.stage.addChild(ani);
+
+        if (aniName == "Idle") {
+            this.idle = ani;
+        } else if (aniName == "Hit") {
+            this.hit = ani;
+        } else if (aniName == "Skill") {
+            this.shill = ani;
+        } else if (aniName == "Walk") {
+            this.walk = ani;
+        } else if (aniName == "Effect") {
+            this.effect = ani;
+        }
     }
-})(window)
+}
