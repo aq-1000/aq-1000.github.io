e69626ce49e58e17b9cefdab7165bce27fe7137f
PanYoucheng <aq_1000@sina.com>
2018-10-31 18:12:18 +0800

    添加战斗

--- SGameDemo/Scripts/main.js
+++ SGameDemo/Scripts/main.js
@@ -1,5 +1,6 @@
 let windowWidth = window.innerWidth, windowHeight = window.innerHeight;
 let gScale = 1.0;
+let BGWidth = 720, BGHeight = 1280;
 
 // Create aPixi Application
 let app = new PIXI.Application({width: windowWidth, height: windowHeight});
@@ -16,22 +17,30 @@ document.body.appendChild(app.view);
 //app.renderer.resize(window.innerWidth, window.innerHeight);
 
 let aladdin = null;
-let aladdin2 = null;
+let sinbad = null;
+let anubis = null;
+let serpentQueen = null;
+let shedu = null;
 let skillUI = null;
 let victory = null;
-let activation = null;
-let target = null;
+let activateArrow = null;
+let targetArrow = null;
+let aniSpeed = 0.06;
+
+let canClick = true;
+let activateRole = null;
+let targetRole = null;
 
 // 加载静态图
 PIXI.loader
     .add("Assets/Images/Background.png")
     .add("Assets/Images/SkillUI.png")
-    .add("Assets/Images/NoSkill.png")
     .add("Assets/Images/Victory.png")
     .add("Assets/Images/BloodBar.png")
     .add("Assets/Images/Activation.png")
     .add("Assets/Images/Target.png")
-    .add("Assets/Images/Roles/Aladdin.json")
+    .add("Assets/Images/Our.json")
+    .add("Assets/Images/Enemy.json")
     .on("progress", function(){
         // do something when loading
     })
@@ -41,50 +50,99 @@ PIXI.loader
 function onAssetsLoaded()
 {
     // 背景图
-    let backgroundSprite = new PIXI.Sprite(PIXI.loader.resources["Assets/Images/Background.png"].texture);
-    app.stage.addChild(backgroundSprite);
+    let background = new PIXI.Sprite(PIXI.loader.resources["Assets/Images/Background.png"].texture);
+    BGWidth = background.width;
+    BGHeight = background.height;
+    app.stage.addChild(background);
 
-    gScale = windowWidth / backgroundSprite.width;  // 调整背景图，铺满全屏，图片居中
-    backgroundSprite.width = windowWidth;
-    backgroundSprite.height = backgroundSprite.height * gScale;
-    if (backgroundSprite.height > windowHeight) 
-    {
-        backgroundSprite.y -= (backgroundSprite.height - windowHeight) / 2;
+    // 调整背景图
+    if (windowWidth > windowHeight) {
+        background.width = windowHeight;
     }
+    else {
+        background.width = windowWidth;
+    }
+    gScale = background.width / BGWidth;
+    background.height = background.height * gScale;
+    background.x += (windowWidth - background.width) * 0.5;     // 图片水平居中
+    background.y -= (background.height - windowHeight);     // 图片底部和窗口持平
 
     // aladdin
-    var sheet = PIXI.loader.resources["Assets/Images/Roles/Aladdin.json"].spritesheet;
-    aladdin = new Role("Aladdin", sheet, windowWidth / 8, windowHeight / 2, gScale, gScale);
-    aladdin.CreateAnimation("Idle", 0.05);
-    aladdin.CreateAnimation("Hit", 0.05);
-    aladdin.CreateAnimation("Skill", 0.1);
-    aladdin.CreateAnimation("Effect", 0.1);
+    var sheet = PIXI.loader.resources["Assets/Images/Our.json"].spritesheet;
+    aladdin = new Role("Aladdin", background, sheet, true, BGWidth * 0.15, BGHeight * 0.557);
+    aladdin.CreateAnimation("Idle", aniSpeed);
+    aladdin.CreateAnimation("Hit", aniSpeed);
+    aladdin.CreateAnimation("Skill", aniSpeed * 2);
+    aladdin.CreateAnimation("Effect", aniSpeed * 4);
     aladdin.CreateSkillUI();
     aladdin.CreateBloodBar(new PIXI.Sprite(PIXI.loader.resources["Assets/Images/BloodBar.png"].texture));
+    aladdin._Idle.click = SelectAladdin;
+    aladdin._Idle.touchend = SelectAladdin;
 
-    // aladdin2
-    sheet = PIXI.loader.resources["Assets/Images/Roles/Aladdin.json"].spritesheet;
-    aladdin2 = new Role("Aladdin", sheet, windowWidth * 7 / 8, windowHeight / 2, -gScale, gScale);
-    aladdin2.CreateAnimation("Idle", 0.05);
-    aladdin2.CreateAnimation("Hit", 0.05);
-    aladdin2.CreateAnimation("Skill", 0.1);
-    aladdin2.CreateAnimation("Effect", 0.1);
-    aladdin2.CreateBloodBar(new PIXI.Sprite(PIXI.loader.resources["Assets/Images/BloodBar.png"].texture));
+    // sinbad
+    sinbad = new Role("Sinbad", background, sheet, true, BGWidth * 0.15, BGHeight * 0.725);
+    sinbad.CreateAnimation("Idle", aniSpeed);
+    sinbad.CreateAnimation("Hit", aniSpeed);
+    sinbad.CreateAnimation("Skill", aniSpeed * 2);
+    sinbad.CreateSkillUI();
+    sinbad.CreateBloodBar(new PIXI.Sprite(PIXI.loader.resources["Assets/Images/BloodBar.png"].texture));
+    sinbad._Idle.click = SelectSinbad;
+    sinbad._Idle.touchend = SelectSinbad;
+
+    // serpentQueen
+    sheet = PIXI.loader.resources["Assets/Images/Enemy.json"].spritesheet;
+    serpentQueen = new Role("SerpentQueen", background, sheet, false, BGWidth * 0.85, BGHeight * 0.5);
+    serpentQueen.CreateAnimation("Idle", aniSpeed);
+    serpentQueen.CreateAnimation("Hit", aniSpeed);
+    serpentQueen.CreateBloodBar(new PIXI.Sprite(PIXI.loader.resources["Assets/Images/BloodBar.png"].texture));
+    serpentQueen._Idle.click = SelectSerpentQueen;
+    serpentQueen._Idle.touchend = SelectSerpentQueen;
+    
+    // anubis
+    anubis = new Role("Anubis", background, sheet, false, BGWidth * 0.85, BGHeight * 0.65);
+    anubis.CreateAnimation("Idle", aniSpeed);
+    anubis.CreateAnimation("Hit", aniSpeed);
+    anubis.CreateAnimation("Skill", aniSpeed * 2);
+    anubis.CreateBloodBar(new PIXI.Sprite(PIXI.loader.resources["Assets/Images/BloodBar.png"].texture));
+    anubis._Idle.click = SelectAnubis;
+    anubis._Idle.touchend = SelectAnubis;
+
+    // shedu
+    shedu = new Role("Shedu", background, sheet, false, BGWidth * 0.85, BGHeight * 0.8);
+    shedu.CreateAnimation("Idle", aniSpeed);
+    shedu.CreateAnimation("Hit", aniSpeed);
+    shedu.CreateBloodBar(new PIXI.Sprite(PIXI.loader.resources["Assets/Images/BloodBar.png"].texture));
+    shedu._Idle.click = SelectShedu;
+    shedu._Idle.touchend = SelectShedu;
 
     // 激活目标
-    activation = new PIXI.Sprite(PIXI.loader.resources["Assets/Images/Activation.png"].texture);
-    target = new PIXI.Sprite(PIXI.loader.resources["Assets/Images/Target.png"].texture);
-    activation.anchor.set(0.5);
-    target.anchor.set(0.5);
-    app.stage.addChild(activation);
-    app.stage.addChild(target);
+    activateArrow = new PIXI.Sprite(PIXI.loader.resources["Assets/Images/Activation.png"].texture);
+    targetArrow = new PIXI.Sprite(PIXI.loader.resources["Assets/Images/Target.png"].texture);
+    activateArrow.anchor.set(0.5);
+    targetArrow.anchor.set(0.5);
+    background.addChild(activateArrow);
+    background.addChild(targetArrow);
 
     // 技能UI
     skillUI = new PIXI.Sprite(PIXI.loader.resources["Assets/Images/SkillUI.png"].texture);
-    skillUI.x = windowWidth - skillUI.width;
-    skillUI.y = windowHeight - skillUI.height;
-    app.stage.addChild(skillUI);
-    UpdateSkillUI(aladdin);
+    skillUI.x = BGWidth - skillUI.width;
+    skillUI.y = BGHeight - skillUI.height;
+    skillUI.interactive = true;
+    skillUI.click = SelectSkill;
+    background.addChild(skillUI);
+
+    // 胜利
+    victory = new PIXI.Sprite(PIXI.loader.resources["Assets/Images/Victory.png"].texture);
+    victory.anchor.set(0.5);
+    victory.x = windowWidth * 0.5;
+    victory.y = windowHeight * 0.5;
+    victory.scale.x = gScale;
+    victory.scale.y = gScale;
+    victory.visible = false;
+    app.stage.addChild(victory);
+
+    SelectAladdin();
+    SelectAnubis();
 }
 
 function UpdateSkillUI(obj)
@@ -92,22 +150,159 @@ function UpdateSkillUI(obj)
     skillUI.removeChildren();
     if (null != obj._SkillUI0) {
         skillUI.addChild(obj._SkillUI0);
-        obj._SkillUI0.x += skillUI.width * 0.2;
-        obj._SkillUI0.y += skillUI.height * 0.65;
+        obj._SkillUI0.x = skillUI.width * 0.08;
+        obj._SkillUI0.y = skillUI.height * 0.378;
     }
     if (null != obj._SkillUI1) {
         skillUI.addChild(obj._SkillUI1);
-        obj._SkillUI1.x = skillUI.width * 0.5;
-        obj._SkillUI1.y += skillUI.height * 0.65;
+        obj._SkillUI1.x = skillUI.width * 0.39;
+        obj._SkillUI1.y = skillUI.height * 0.378;
     }
     if (null != obj._SkillUI2) {
         skillUI.addChild(obj._SkillUI2);
-        obj._SkillUI2.x += skillUI.width * 0.8;
-        obj._SkillUI2.y += skillUI.height * 0.65;
+        obj._SkillUI2.x = skillUI.width * 0.697;
+        obj._SkillUI2.y = skillUI.height * 0.378;
     }
 
-    activation.x = obj._X;
-    activation.y = obj._Y - obj._Idle.height * 0.25;
-    target.x = aladdin2._X;
-    target.y = aladdin2._Y - aladdin2._Idle.height * 0.25;
+    activateArrow.x = obj._X;
+    activateArrow.y = obj._Y - obj._Idle.height * 0.33;
+}
+
+function SelectAladdin()
+{
+    if (!canClick) {
+        return;
+    }
+    activateRole = aladdin;
+    UpdateSkillUI(activateRole);
+}
+
+function SelectSinbad()
+{
+    if (!canClick) {
+        return;
+    }
+    activateRole = sinbad;
+    UpdateSkillUI(activateRole);
+}
+
+function SelectSerpentQueen()
+{
+    if (!canClick) {
+        return;
+    }
+    targetRole = serpentQueen;
+    targetArrow.x = targetRole._X;
+    targetArrow.y = targetRole._Y - targetRole._Idle.height * 0.29;
+}
+
+function SelectAnubis()
+{
+    if (!canClick) {
+        return;
+    }
+    targetRole = anubis;
+    targetArrow.x = targetRole._X;
+    targetArrow.y = targetRole._Y - targetRole._Idle.height * 0.29;
+}
+
+function SelectShedu()
+{
+    if (!canClick) {
+        return;
+    }
+    targetRole = shedu;
+    targetArrow.x = targetRole._X;
+    targetArrow.y = targetRole._Y - targetRole._Idle.height * 0.29;
+}
+
+function SelectSkill()
+{
+    if (!canClick) {
+        return;
+    }
+    canClick = false;
+    activateRole.Skill();
+    activateRole._Skill.onComplete = function(){ 
+        activateRole._Skill.visible = false; 
+        activateRole._Idle.visible = true;
+        activateRole._Idle.gotoAndPlay(0);
+        PlayOurEffect();  // 特效
+    };
+}
+
+// Setup the animation loop.
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
+function PlayOurEffect()
+{
+    aladdin._Effect.visible = true;
+    aladdin._Effect.gotoAndPlay(0);
+    var coords = { x: activateRole._X, y: activateRole._Y }; // Start at (0, 0)
+    var tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
+        .to({ x: targetRole._X, y: targetRole._Y }, 500) // Move to (300, 200) in 1 second.
+        .easing(TWEEN.Easing.Linear.None) // Use an easing function to make the animation smooth.
+        .onUpdate(function() { // Called after tween.js updates 'coords'.
+            // Move 'box' to the position described by 'coords' with a CSS translation.
+        //    box.style.setProperty('transform', 'translate(' + coords.x + 'px, ' + coords.y + 'px)');
+            aladdin._Effect.x = coords.x;
+            aladdin._Effect.y = coords.y;
+        })
+        .onComplete(function() {
+            TargetHit();
+            aladdin._Effect.visible = false;
+        })
+        .start(); // Start the tween immediately.
+}
+
+function PlayEnemyEffect()
+{
+    aladdin._Effect.visible = true;
+    aladdin._Effect.gotoAndPlay(0);
+    var coords = { x: anubis._X, y: anubis._Y }; // Start at (0, 0)
+    var tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
+        .to({ x: activateRole._X, y: activateRole._Y }, 500) // Move to (300, 200) in 1 second.
+        .easing(TWEEN.Easing.Linear.None) // Use an easing function to make the animation smooth.
+        .onUpdate(function() { // Called after tween.js updates 'coords'.
+            // Move 'box' to the position described by 'coords' with a CSS translation.
+        //    box.style.setProperty('transform', 'translate(' + coords.x + 'px, ' + coords.y + 'px)');
+            aladdin._Effect.x = coords.x;
+            aladdin._Effect.y = coords.y;
+        })
+        .onComplete(function() {
+            aladdin._Effect.visible = false;
+
+            anubis._Skill.visible = false; 
+            anubis._Idle.visible = true;
+            activateRole.Hit();
+            canClick = true;
+        })
+        .start(); // Start the tween immediately.
+}
+
+function TargetHit()
+{
+    targetRole.Hit();
+    targetRole._Hit.onComplete = function(){ 
+        targetRole._Hit.visible = false; 
+        targetRole._Idle.visible = true;
+        targetRole._Idle.gotoAndPlay(0);
+        
+        // anubis反击
+        if (anubis._Idle.visible)
+        {
+            anubis.Skill();
+            anubis._Skill.onComplete = function(){ 
+                anubis._Skill.visible = false; 
+                anubis._Idle.visible = true;
+                PlayEnemyEffect();
+            };
+        } else {
+            canClick = true;
+        }
+    };
 }
--- SGameDemo/Scripts/role.js
+++ SGameDemo/Scripts/role.js
@@ -1,22 +1,23 @@
-function Role(name, sheet, x, y, scaleX, scaleY){
+function Role(name, parent, sheet, our, x, y){
     this._Name = name;
+    this._Parent = parent;
     this._Sheet = sheet;
+    this._Our = our;
     this._X = x;
     this._Y = y;
-    this._ScaleX = scaleX;
-    this._ScaleY = scaleY;
 }
 
 Role.prototype = {
-    _This:this,
     _Name:"",
+    _Parent:null,
     _Sheet:null,
+    _Our:true,
     _X:0,
     _Y:0,
-    _Scale:1.0,
     _Idle:null,
     _Hit:null,
     _Skill:null,
+    _Dead:null,
     _Walk:null,
     _Effect:null,
     _SkillUI0:null,
@@ -38,9 +39,9 @@ Role.prototype = {
         if (typeof this._Walk != "undefined" && this._Walk != null) {
             this._Walk.visible = false;
         }
-        if (typeof this._Effect != "undefined" && this._Effect != null) {
-            this._Effect.visible = false;
-        }
+        // if (typeof this._Effect != "undefined" && this._Effect != null) {
+        //     this._Effect.visible = false;
+        // }
     },
 
     Hit:function()
@@ -48,11 +49,13 @@ Role.prototype = {
         this.Hide();
         this._Hit.visible = true;
         this._Hit.gotoAndPlay(0);
-        this._Hit.onComplete = function(){ 
-            this._Hit.visible = false; 
-            this._Idle.visible = true;
-            this._Idle.gotoAndPlay(0);
-        }.bind(this);
+        if (this._Our) {
+            this._Hit.onComplete = function(){ 
+                this._Hit.visible = false; 
+                this._Idle.visible = true;
+                this._Idle.gotoAndPlay(0);
+            }.bind(this);
+        }
     },
 
     Skill:function()
@@ -60,11 +63,6 @@ Role.prototype = {
         this.Hide();
         this._Skill.visible = true;
         this._Skill.gotoAndPlay(0);
-        this._Skill.onComplete = function(){ 
-            this._Skill.visible = false; 
-            this._Idle.visible = true;
-            this._Idle.gotoAndPlay(0);
-        }.bind(this);
     },
 
     CreateAnimation:function(aniName, speed)
@@ -74,37 +72,39 @@ Role.prototype = {
     //    var.onComplete = function(){ aladdinIdle.visible = false; };
         ani.x = this._X;
         ani.y = this._Y;
-        ani.scale.x = this._ScaleX;
-        ani.scale.y = this._ScaleY;
+        if (!this._Our) {
+            ani.scale.x = -1;
+        }
         ani.anchor.set(0.5);
         ani.visible = false;
         ani.loop = false;
         ani.gotoAndPlay(0);
-        app.stage.addChild(ani);
+        this._Parent.addChild(ani);
 
         if (aniName == "Idle") {
             ani.visible = true;
             ani.loop = true;
             this._Idle = ani;
             this._Idle.interactive = true;
-            this._Idle.click = this.Skill.bind(this);
-            this._Idle.touchend = this.Skill.bind(this);
         } else if (aniName == "Hit") {
             this._Hit = ani;
         } else if (aniName == "Skill") {
             this._Skill = ani;
+        } else if (aniName == "Dead") {
+            this._Dead = ani;
         } else if (aniName == "Walk") {
             this._Walk = ani;
         } else if (aniName == "Effect") {
             this._Effect = ani;
+            this._Effect.loop = true;
         }
     },
 
     CreateSkillUI:function()
     {
-        this._SkillUI0 = new PIXI.Sprite(this._Sheet.textures[this.Name + "_Skill_UI0.png"]);
-        this._SkillUI1 = new PIXI.Sprite(this._Sheet.textures[this.Name + "_Skill_UI1.png"]);
-        this._SkillUI2 = new PIXI.Sprite(this._Sheet.textures[this.Name + "_Skill_UI2.png"]);
+        this._SkillUI0 = new PIXI.Sprite(this._Sheet.textures[this._Name + "_UI0.png"]);
+        this._SkillUI1 = new PIXI.Sprite(this._Sheet.textures[this._Name + "_UI1.png"]);
+        this._SkillUI2 = new PIXI.Sprite(this._Sheet.textures[this._Name + "_UI2.png"]);
     },
 
     CreateBloodBar:function(sprite)
@@ -113,6 +113,6 @@ Role.prototype = {
         this._BloodBar.anchor.set(0.5);
         this._BloodBar.x = this._X;
         this._BloodBar.y = this._Y - this._Idle.height * 0.2;
-        app.stage.addChild(this._BloodBar);
+        this._Parent.addChild(this._BloodBar);
     },
 }
